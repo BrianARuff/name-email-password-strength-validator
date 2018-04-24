944d1477d4338d5b01fff54b313ffa57509f1652
thelonewolf198927 <thelonewolf198927@gmail.com>
2018-04-23 22:43:21 -0400

    centered text

--- src/components/JoinForm.js
+++ src/components/JoinForm.js
@@ -27,7 +27,7 @@ class JoinForm extends Component {
                 <form action="/" method="POST" noValidate>
 
                     <div className="d-flex flex-row justify-content-between align-items-center px-3 mb-5">
-                        <legend className="form-label mb-0">Password Quality</legend>
+                        <legend className="form-label mb-0 text-center">Password Quality</legend>
                         { formValidated && <button type="button" className="btn btn-primary text-uppercase px-3 py-2">Join</button> }
                     </div>
 
